23371bbbf981f2ece6a5c18eeb5d35086f884d2e
rxlabz <dev@rxlabz.com>
2017-05-12 15:15:45 +0200

    fix cast issue => Now : cannot load underlying module...

--- ios/Classes/SwiftPluginPoc.swift
+++ ios/Classes/SwiftPluginPoc.swift
@@ -1,36 +1,14 @@
-/*import Foundation
-import Flutter
-
-@objc public class SwiftPluginPocPlugin:NSObject{
-    
-    var channel:FlutterMethodChannel
-    
-    public init(withController controller:FlutterViewController){
-        channel = FlutterMethodChannel(name: "swift_plugin_poc", binaryMessenger: controller)
-        super.init()
-        channel.setMethodCallHandler(onMethodCall)
-    }
-    
-    public func onMethodCall( _ call:FlutterMethodCall, result:FlutterResult ){
-        if call.method == "getPlatformVersion" {
-            result("iOS response : \(UIDevice.current.systemVersion)")
-        } else {
-            result( FlutterMethodNotImplemented)
-        }
-    }
-}*/
-
 import Foundation
 import Flutter
 
-@objc public class SwiftPluginPocPlugin:NSObject{
+@objc public class SwiftPluginPocPlugin:NSObject, FlutterPlugin{
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "swift_plugin_poc", binaryMessenger: registrar.messenger())
         let instance:SwiftPluginPocPlugin  = SwiftPluginPocPlugin()
-        registrar.addMethodCallDelegate(instance as! FlutterPlugin, channel:channel)
+        registrar.addMethodCallDelegate(instance, channel:channel)
     }
     
-    public func handle( _ call:FlutterMethodCall, result:FlutterResult ){
+    public func handle( _ call:FlutterMethodCall, result: @escaping FlutterResult ){
         if call.method == "getPlatformVersion" {
             result("iOS response : \(UIDevice.current.systemVersion)")
         } else {
